b3fe022fe17976551d8df41ac51d6b32ad8d8c77
Sumit Mukherjee <48638545+mukherjeeSumit@users.noreply.github.com>
2023-10-01 18:11:22 +0530

    Update and rename 5-Leetcode-607-Sales-Person.sql to 05_Leetcode_607_Sales_Person.sql

--- 05_Leetcode_607_Sales_Person.sql
+++ 05_Leetcode_607_Sales_Person.sql
@@ -3,6 +3,6 @@ FROM SalesPerson s
 WHERE s.name NOT IN
     (SELECT s.name
     FROM SalesPerson s
-         join Orders o ON s.sales_id = o.sales_id
-         join Company c ON o.com_id = c.com_id
+         JOIN Orders o ON s.sales_id = o.sales_id
+         JOIN Company c ON o.com_id = c.com_id
     WHERE c.name = 'Red')
