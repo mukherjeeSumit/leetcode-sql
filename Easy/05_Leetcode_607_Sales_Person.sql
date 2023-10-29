d0bf08c77dc4008b76ea3f82b67aa8dd6f922f22
Sumit Mukherjee <48638545+mukherjeeSumit@users.noreply.github.com>
2023-10-30 01:54:19 +0530

    Update 05_Leetcode_607_Sales_Person.sql

--- Easy/05_Leetcode_607_Sales_Person.sql
+++ Easy/05_Leetcode_607_Sales_Person.sql
@@ -1,8 +1,10 @@
 SELECT s.name
 FROM SalesPerson s
-WHERE s.name NOT IN
-    (SELECT s.name
-    FROM SalesPerson s
-         JOIN Orders o ON s.sales_id = o.sales_id
-         JOIN Company c ON o.com_id = c.com_id
-    WHERE c.name = 'Red')
+WHERE s.name NOT IN 
+    (
+        SELECT s.name
+        FROM SalesPerson s
+        JOIN Orders o ON s.sales_id = o.sales_id
+        JOIN Company c ON o.com_id = c.com_id
+        WHERE c.name = 'Red'
+    )
